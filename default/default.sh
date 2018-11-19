86f4c3ded6166fd8927ac250418eaea9501e7a76
chawdhary <44389314+chawdhary@users.noreply.github.com>
2018-11-19 12:25:24 +0500

    Create default.sh

---
+++ default/default.sh
@@ -0,0 +1,6 @@
+echo ' ur terminal change into default terminal'
+sleep 10
+rm /data/data/com.termux/files/usr/etc/bash.bashrc
+mv /$HOME/Terminal-Master/default/bash.bashrc /data/data/com.termux/files/usr/etc
+   echo ' successfuly done '
+   echo 'plz restart ur termux'
